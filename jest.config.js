4948cd7d24d6da98c55e4e708f7c72794b0285be
NsdHSO <nechiforelsamuel@gmail.com>
2026-01-28 16:31:35 +0200

    feat: 🎸 added pnpm package manager

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@shopify/flash-list|nativewind)',
-  ],
+  transformIgnorePatterns: [],
   testMatch: [
     '**/__tests__/**/*.{js,jsx,ts,tsx}',
     '**/*.{spec,test}.{js,jsx,ts,tsx}',
